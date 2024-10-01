e8cdf9493e7dcef7778af701886cebb7248c5604
Bathala <S566466@nwmissouri.edu>
2024-10-01 10:43:24 -0500

    gdujcgd

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,8 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    baseUrl: 'http://localhost:3000', // Change this to your app's base URL
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    // Your e2e configuration
+    baseUrl: 'http://localhost:3000', // Example base URL
   },
-})
+});
